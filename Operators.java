5d26fffec563de390550d413b148ed59fe9f0a18
Devansh <72754838+Devansh220805@users.noreply.github.com>
2026-04-02 18:17:28 +0000

    Operators

---
+++ Operators.java
@@ -0,0 +1,18 @@
+public class Operators{
+    public static void main(String[] args){
+        int a = 10;
+        int b = 20;
+
+        // Arithmetic Operators : +, -, *, /, %
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+
+        // Assignment Operators : =, +=, -=, *=, /= 
+        int c = 5;
+        c += a; //15 output
+        System.out.println(c);
+        
+    }
+}
