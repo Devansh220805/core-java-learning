eb345533cbe55406641b9ae032cf0fe723f2717c
Devansh <72754838+Devansh220805@users.noreply.github.com>
2026-04-02 15:19:45 +0000

    variable & datatype

---
+++ Java_basic.java
@@ -0,0 +1,21 @@
+public class Java_basic{
+    public static void main(String[] args){
+        System.out.println("Hello world!");// print statement
+
+        // variable = container to store data 
+        int age = 20;
+        // data type =  that defines the type of value a variable can hold
+        /* int : whole number
+           long : store larger whole number then int 
+           short : range of number is short then int 
+           double : decimal value 
+           char : single character 
+           boolean : store true/false only 
+           float : floting point number 
+           byte : it can store 1 byte(8 bits)
+            
+        */      
+
+
+    }
+}
