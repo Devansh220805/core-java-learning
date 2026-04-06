7cb9f49ed10389eebc6d2d32521d89973ae9ba06
Devansh <72754838+Devansh220805@users.noreply.github.com>
2026-04-06 16:59:24 +0000

    Arrays

---
+++ Arrays.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Arrays {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        // Syntax
+        int[] number = new int[5];
+        number[0] = 40;
+        number[1] = 50;
+
+        int[] marks = {97,98,85,89};
+        
+
+ 
+    }
+    
+}
