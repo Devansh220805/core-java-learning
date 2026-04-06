6f4822d85fc935625c178e2a07e4c64a6ddb911e
Devansh <72754838+Devansh220805@users.noreply.github.com>
2026-04-06 17:38:14 +0000

    Array

--- Arrays.java
+++ Arrays.java
@@ -7,11 +7,43 @@ public class Arrays {
         int[] number = new int[5];
         number[0] = 40;
         number[1] = 50;
-
+        // Or
         int[] marks = {97,98,85,89};
         
+        // how to print Arrays
+        for(int i = 0; i < number.length; i++){
+            System.out.println("Index : "+i +"| Values : "+number[i]);
+        }
 
+        for(int i=0; i < marks.length; i++){
+            System.out.println("Index : "+i+" | Values : "+ marks[i]);
+        }
  
+
+        // Q1. How to take input from useer in Array
+        System.out.print("Enter number of value you want to enter :\t");
+        int size = sc.nextInt();
+        int no[]=new int[size];
+
+        for(int i=0; i<size; i++){
+            System.out.print("\nEnter "+(i+1)+" value :\t");
+            no[i]=sc.nextInt();
+        }
+        // for(int i=0; i < no.length; i++){
+        //     System.out.println("Index : "+i+" | Values : "+ no[i]);
+        // }
+
+        // Tacke an array as input from user Search for a given no X and print the index at which it occurs
+        System.out.print("Enter number You want to Find in array :\t");
+        int fno = sc.nextInt();
+
+        if(size !=0){
+            for(int i=0; i<size; i++){
+                if(fno == no[i]){
+                    System.out.println("\n Number found at index :\t"+i);
+                    break;
+                }
+            }
+        }
     }
-    
 }
